1ad37a0b12ce047f868d26e8133a79e4f93a6300
Dat Trxng <datkroos2709@gmail.com>
2024-11-18 15:34:45 +0700

    update PlayScreen

--- app/src/main/java/com/example/harmonyhub/ui/play/PlayScreen.kt
+++ app/src/main/java/com/example/harmonyhub/ui/play/PlayScreen.kt
@@ -1,172 +1,268 @@
 package com.example.harmonyhub.ui.play
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.harmonyhub.R
+import com.example.harmonyhub.ui.theme.NotoSans
+
+@Composable
+fun RoundedImageCard(
+    imageResId: Int,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.Gray)
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = "Song Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
 
 @Composable
 fun PlayScreen(
     modifier: Modifier = Modifier
 ) {
-    var isPlaying by remember { mutableStateOf(false) } // Trạng thái phát nhạc
+    var isPlaying by remember { mutableStateOf(false) }
 
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Black)
             .padding(16.dp),
-        verticalArrangement = Arrangement.Top
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        item {
-            // Header with dropdown and options
-            Row(
-                modifier = Modifier
-                    .padding(vertical = 16.dp)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = { /* Add back action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
             ) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowDropDown,
-                    contentDescription = "arrowDropDown",
-                    modifier = Modifier.size(24.dp)
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Text(
-                    text = "PLAYING NOW",
-                    maxLines = 1,
-                    fontSize = 16.sp
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Filled.MoreVert,
-                    contentDescription = "moreVert",
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_arrow_down_90),
+                    contentDescription = "Back",
                     modifier = Modifier.size(24.dp)
                 )
             }
 
-            Spacer(modifier = Modifier.size(30.dp))
 
-            // Song card with image
-            SongCard()
 
-            Spacer(modifier = Modifier.size(16.dp))
+            Button(
+                onClick = { /* More options */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_more_90),
+                    contentDescription = "More options",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
 
-            // Song details and controls
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Song image
+        RoundedImageCard(
+            imageResId = R.drawable.v,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+        )
+
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.weight(1f)
             ) {
-                Text(text = "Song Title", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-                Text(text = "Artist Name", fontSize = 16.sp, color = Color.Gray)
-
-                Spacer(modifier = Modifier.size(20.dp))
-
-                // Progress bar and timestamp
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Slider(
-                        value = 0.3f, // Giá trị giả định cho thanh tiến trình
-                        onValueChange = {},
-                        colors = SliderDefaults.colors(
-                            thumbColor = MaterialTheme.colorScheme.primary,
-                            activeTrackColor = MaterialTheme.colorScheme.primary
-                        ),
-                        modifier = Modifier
-                            .fillMaxWidth(0.8f)
+                Text(
+                    text = "Song Title",
+                    style = TextStyle(
+                        fontFamily = NotoSans,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = Color.White
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth(0.8f),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(text = "1:15")
-                        Text(text = "3:45")
-                    }
-                }
+                )
 
-                Spacer(modifier = Modifier.size(20.dp))
+                Text(
+                    text = "Artist Name",
+                    style = TextStyle(
+                        fontFamily = NotoSans,
+                        fontSize = 18.sp,
+                        color = Color.Gray
+                    )
+                )
+            }
 
-                // Playback control buttons
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    IconButton(onClick = { /* Previous song action */ }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Previous",
-                            modifier = Modifier.size(40.dp)
-                        )
-                    }
+            Spacer(modifier = Modifier.width(8.dp))
 
-                    Spacer(modifier = Modifier.size(16.dp))
-
-                    IconButton(onClick = { isPlaying = !isPlaying }) {
-                        Icon(
-                            imageVector = if (isPlaying) Icons.Filled.Lock else Icons.Filled.PlayArrow,
-                            contentDescription = "Play/Pause",
-                            modifier = Modifier
-                                .size(64.dp)
-                                .padding(8.dp)
-                                .clip(CircleShape)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.size(16.dp))
-
-                    IconButton(onClick = { /* Next song action */ }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
-                            contentDescription = "Next",
-                            modifier = Modifier.size(40.dp)
-                        )
-                    }
-                }
+            Button(
+                onClick = { /* Add favorite action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_heart_90),
+                    contentDescription = "Favorite",
+                    modifier = Modifier.size(30.dp)
+                )
             }
         }
-    }
-}
 
-@Composable
-fun SongCard(
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = Modifier
-            .size(400.dp),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Box(contentAlignment = Alignment.Center) {
-            Image(
-                painter = painterResource(id = R.drawable.v),
-                contentDescription = "songImage",
-                modifier = Modifier.fillMaxSize()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Slider(
+                value = 0.3f,
+                onValueChange = {},
+                colors = SliderDefaults.colors(
+                    thumbColor = Color.White,
+                    activeTrackColor = Color.White,
+                    inactiveTrackColor = Color.Gray
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
             )
+            Row(
+                modifier = Modifier.fillMaxWidth(0.8f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "0:25", fontSize = 12.sp, color = Color.White)
+                Text(text = "3:15", fontSize = 12.sp, color = Color.White)
+            }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(
+                onClick = { /* Shuffle action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_shuffle_96),
+                    contentDescription = "Shuffle",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Previous button
+            Button(
+                onClick = { /* Previous song action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_skip_to_start_90),
+                    contentDescription = "Previous",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Play/Pause button
+            Button(
+                onClick = { isPlaying = !isPlaying },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = if (isPlaying) R.drawable.icons8_pause_50 else R.drawable.icons8_circled_play_64
+                    ),
+                    contentDescription = "Play/Pause",
+                    modifier = Modifier.size(64.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Next button
+            Button(
+                onClick = { /* Next song action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_next_90),
+                    contentDescription = "Next",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+            // Repeat button
+            Button(
+                onClick = { /* Repeat action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_repeat_96),
+                    contentDescription = "Repeat",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+        }
+
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun PlayScreenPreview() {
-    PlayScreen()
+    MaterialTheme {
+        PlayScreen()
+    }
 }
